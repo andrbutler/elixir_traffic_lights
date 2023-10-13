10c37c1c6c95dd0c96ac95cdc96ac231b0b2257f
ol_ghosty <olghosty.software@gmail.com>
2023-10-12 23:17:59 -0400

    Add TrafficGrid Genserver module.

--- lib/traffic_lights.ex
+++ lib/traffic_lights.ex
@@ -8,7 +8,7 @@ defmodule TrafficLights do
   """
   defmodule Light do
     def start_link(_args) do
-      GenServer.start_link(__Module__, [])
+      GenServer.start_link(__MODULE__, [])
     end
 
     def change(start_color) do
@@ -24,7 +24,7 @@ defmodule TrafficLights do
     end
 
     def current_light(pid) do
-      GenServer.call(pid, _from, :get)
+      GenServer.call(pid, :get)
     end
 
     @impl true
@@ -33,16 +33,59 @@ defmodule TrafficLights do
     end
 
     @impl true
-    def handle_call(:transition, state) do
+    def handle_cast(:transition, state) do
       {:noreply, change(state)}
     end
 
     @impl true
-    def handle_cast(:get, _from, state) do
+    def handle_call(:get, _from, state) do
       {:reply, state, state}
     end
   end
 
-  defmodule Traffic_grid do
+  defmodule TrafficGrid do
+
+    def start_link(_args) do
+      GenServer.start_link(__MODULE__, [])
+    end
+
+    def transition(pid) do
+      GenServer.cast(pid, :transition)
+    end
+
+    def current_lights(pid) do
+      GenServer.call(pid, :get_light_colors)
+    end
+
+    def get_lights(pid) do
+      GenServer.call(pid, :get_lights)
+    end
+
+    @impl true
+    def init(_args) do
+      list = Enum.map(1..6, fn _ -> 
+        {:ok, pid} = Light.start_link([])
+        pid
+      end)
+      {:ok, list}
+    end
+
+    @impl true
+    def handle_cast(:transition, state) do
+      Enum.map(state, fn pid -> Light.transition(pid) end)
+      {:noreply, state}
+    end
+
+    @impl true
+    def handle_call(:get_light_colors, _from, state) do
+      colors = Enum.map(state, fn pid -> Light.current_light(pid) end)
+      {:reply, colors, state}
+    end
+
+    @impl true
+    def handle_call(:get_lights, _from, state) do
+      {:reply, state, state}
+    end
+
   end
 end
