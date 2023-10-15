89276feba8dc05032f55d6eefbd01ec8600ac957
ol_ghosty <olghosty.software@gmail.com>
2023-10-14 23:58:38 -0400

    add docs, doctests, and begin adding unit tests.

--- lib/traffic_lights.ex
+++ lib/traffic_lights.ex
@@ -1,16 +1,36 @@
 defmodule TrafficLights do
   @moduledoc """
   Documentation for `TrafficLights`.
-  """
-  use GenServer
-
-  @doc """
+  Genserver based implementation of a simple traffic light and a collection of traffic lights.
   """
   defmodule Light do
+
+    use GenServer
+
+    @moduledoc """
+    Documentation for 'Light' when started represents a single traffic light with a persistant state containing its current color.
+    """
+
+    #Client Functions
+
+    @doc """
+    starts the server and initalizes the lights color as green.
+    """
     def start_link(_args) do
       GenServer.start_link(__MODULE__, [])
     end
 
+    @doc """
+    helper function that changes the color of the light in sequence green -> yellow -> red -> green...
+
+    ## Examples
+    iex> change(:green)
+    :yellow
+    iex> change(:yellow)
+    :red
+    iex> change(:red)
+    :green
+    """
     def change(start_color) do
       case start_color do
         :green -> :yellow
@@ -19,13 +39,26 @@ defmodule TrafficLights do
       end
     end
 
+    @doc """
+    calls server and update state using change function.
+    """
     def transition(pid) do
       GenServer.cast(pid, :transition)
     end
 
+    @doc """
+    calls server and retrives current light color.
+
+    ##Examples 
+    iex> {:ok, pid} = Light.start_link([])
+    iex> Light.current_light(pid)
+    :green
+    """
     def current_light(pid) do
       GenServer.call(pid, :get)
     end
+
+    #Server Functions
 
     @impl true
     def init(_args) do
@@ -45,21 +78,47 @@ defmodule TrafficLights do
 
   defmodule TrafficGrid do
 
+    use GenServer
+
+    @moduledoc """
+    Documentation for 'TrafficGrid' when started represents a grid of 5 traffic lights with a persistant state containing their current colors.
+    """
+
+    #Client Functions
+
+    @doc """
+    starts the server and initalizes a list of 5 lights.
+    """
     def start_link(_args) do
       GenServer.start_link(__MODULE__, [])
     end
 
+    @doc """
+    calls server and transitions all lights using logic in Light module.
+    """
     def transition(pid) do
       GenServer.cast(pid, :transition)
     end
 
+    @doc """
+    calls server and returns current color of all lights as a list
+    ##Examples
+    iex> {:ok, pid} = TrafficGrid.start_link([])
+    iex> TrafficGrid.current_lights(pid)
+    [:green, :green, :green, :green, :green]
+    """
     def current_lights(pid) do
       GenServer.call(pid, :get_light_colors)
     end
 
+    @doc """
+    calls server and returns a list of the pids of all lights(can be used to individually call or transition a light in the grid using the Light module.)
+    """
     def get_lights(pid) do
       GenServer.call(pid, :get_lights)
     end
+
+    #Server Functions
 
     @impl true
     def init(_args) do
--- test/traffic_lights_test.exs
+++ test/traffic_lights_test.exs
@@ -2,7 +2,29 @@ defmodule TrafficLightsTest do
   use ExUnit.Case
   doctest TrafficLights
 
-  test "greets the world" do
-    assert TrafficLights.hello() == :world
+  test "verify light sequence-inital" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    assert TrafficLights.Light.current_light(pid) == :green
+  end
+
+  test "verify light sequence-first transition" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    TrafficLights.Light.transition(pid)
+    assert TrafficLights.Light.current_light(pid) == :yellow
+  end
+
+  test "verify light sequence-second transition" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    TrafficLights.Light.transition(pid)
+    TrafficLights.Light.transition(pid)
+    assert TrafficLights.Light.current_light(pid) == :red
+  end
+
+  test "verify light sequence-cycle repeats" do
+    {:ok, pid} = TrafficLights.Light.start_link([])
+    TrafficLights.Light.transition(pid)
+    TrafficLights.Light.transition(pid)
+    TrafficLights.Light.transition(pid)
+    assert TrafficLights.Light.current_light(pid) == :green
   end
 end
